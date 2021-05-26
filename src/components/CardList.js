7262e0f320567b1dbfd29de6bca32394e2dfb1ab
Ori Sayag <sayagori@gmail.com>
2021-05-26 11:31:48 +0300

    first commit

---
+++ src/components/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card.js';
+
+
+const CardList = ({robots}) => {
+  
+  return (
+    <div>
+      {robots.map((robot, i) => {
+        return <Card id={robot.id} name={robot.name} email={robot.email} key={i}/>
+      })}
+    </div>
+  )
+}
+
+export default CardList;
